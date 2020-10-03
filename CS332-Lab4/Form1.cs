c11aed16664a12e64b79377f6c472034d1cdc8bb
Tereshchenko Vlad <ddiavall@yandex.ru>
2020-10-03 21:42:38 +0300

    Changed the drawing of lines and added drawing of points.

--- CS332-Lab4/Form1.cs
+++ CS332-Lab4/Form1.cs
@@ -13,13 +13,14 @@ namespace CS332_Lab4
     public partial class Form1 : Form
     {
         private Graphics g;
-        private bool isLine, isPolygon, isAroundCenter, isScaleAroundCenter;    //переменные для событий
+        private bool isLine, isPolygon, isDot, isAroundCenter, isScaleAroundCenter;    //переменные для событий
         private PointF startPoint, endPoint = PointF.Empty;     //отрисовка линий
         private PointF mainPoint = new PointF(0, 0);
         private PointF minPolyPoint, maxPolyPoint, minEdgePoint, maxEdgePoint;
 
         private PointF[] edge = new PointF[2];
         private PointF[] polygon = new PointF[0];
+        private PointF[] dot = new PointF[0];
 
         private int offsetX = 0, offsetY = 0;   //для поворотов и перемещений
         double rotateAngle = 0, scaleX, scaleY;
@@ -32,12 +33,27 @@ namespace CS332_Lab4
             pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             g = Graphics.FromImage(pictureBox1.Image);
             g.Clear(Color.White);
-        }
+        }  
 
         private void button1_Click(object sender, EventArgs e)
         {
+            isDot = false;
             isLine = false;
             isPolygon = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isDot = false;
+            isLine = true;
+            isPolygon = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            isDot = true;
+            isLine = false;
+            isPolygon = false;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -60,6 +76,10 @@ namespace CS332_Lab4
                     Array.Resize(ref polygon, 1);
                     polygon[0] = startPoint;
                 }
+                else if (isDot)
+                {
+                    startPoint = e.Location;
+                }
             }
         }
 
@@ -69,8 +89,8 @@ namespace CS332_Lab4
             {
                 if (isLine)
                 {
-                    edge[0] = new PointF(startPoint.X, startPoint.Y);
-                    edge[1] = new PointF(endPoint.X, endPoint.Y);
+                    edge[edge.Length - 2] = new PointF(startPoint.X, startPoint.Y);
+                    edge[edge.Length - 1] = new PointF(endPoint.X, endPoint.Y);
                     Array.Resize(ref edge, edge.Length + 2);
 
                     FindMin(ref minEdgePoint, endPoint);
@@ -102,6 +122,11 @@ namespace CS332_Lab4
 
                     polygon[polygon.Length - 1] = endPoint;
                     startPoint = endPoint;
+                }
+                else if (isDot)
+                {
+                    Array.Resize(ref dot, dot.Length + 1);
+                    dot[dot.Length - 1] = startPoint;
                 }
             }
             else
@@ -162,10 +187,16 @@ namespace CS332_Lab4
                 e.Graphics.DrawLine(Pens.Red, edge[i], edge[i + 1]);
             }
 
+            Brush aBrush = (Brush)Brushes.Red;
+            for (int i = 0; i < dot.Length; i ++)
+            {
+                e.Graphics.FillRectangle(aBrush, dot[i].X, dot[i].Y, 1, 1);
+            }
+
             if (polygon.Length > 1)
                 e.Graphics.DrawPolygon(Pens.Red, polygon);
 
-            e.Graphics.DrawLine(Pens.Red, startPoint, endPoint);
+            //e.Graphics.DrawLine(Pens.Red, startPoint, endPoint);
         }
 
         //Очистка picturebox'a.
@@ -175,6 +206,8 @@ namespace CS332_Lab4
             startPoint = endPoint = Point.Empty;
             Array.Clear(edge, 0, edge.Length);
             Array.Clear(polygon, 0, polygon.Length);
+            Array.Clear(dot, 0, dot.Length);
+            Array.Resize(ref dot, 0);
             Array.Resize(ref edge, 2);
             Array.Resize(ref polygon, 0);
             intersection = new PointF(-1, -1);
